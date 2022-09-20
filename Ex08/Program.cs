bdff4f65cb71f0eb71558262df8432389769fe9b
Olga Lobanova <arapova@list.ru>
2022-09-20 21:53:51 +0300

    Задача 08

---
+++ Ex08/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int b = 2;
+string result = "";
+while (b <= a)
+{
+    if (b <= a - 2) result += b + ", ";
+    else result += b;
+    b += 2;
+}
+Console.WriteLine(result);
