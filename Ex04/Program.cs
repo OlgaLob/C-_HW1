110910527c054a7cb2b9d70de4bf70c86a66b07f
Olga Lobanova <arapova@list.ru>
2022-09-20 21:25:38 +0300

    Задача 04

---
+++ Ex04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine() ?? "0");
+int max = int.MinValue;
+max = max > a ? max : a;
+if (max > a) max = max;
+else max = a;
+max = max > b ? max : b;
+max = max > c ? max : c;
+Console.WriteLine(("Максимальное значение ") + (max));
