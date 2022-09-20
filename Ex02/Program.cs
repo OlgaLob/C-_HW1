3b54f6d97b24ddd39813a986725772b80fa5a2b9
Olga Lobanova <arapova@list.ru>
2022-09-20 20:53:23 +0300

    Решение задачи 2

---
+++ Ex02/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+if (a > b) Console.Write($"max == {a} min =={b}");
+else if (a == b) Console.Write($"Числа равны");
+else Console.Write($"max == {b} min =={a}");
