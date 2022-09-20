a1509627c602fe8375dca990418e002d3ccf4d58
Olga Lobanova <arapova@list.ru>
2022-09-20 21:35:43 +0300

    Задача 06

---
+++ Ex06/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+if (a % 2 == 0) Console.WriteLine($"Число {a} - четное");
+else Console.WriteLine($"Число {a} - нечетное");
